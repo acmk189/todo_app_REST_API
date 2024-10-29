a76becd5306d41c0cc57c82060cf07c9beb05824
Kohei Matsuo <koheiihphone@icloud.com>
2024-10-29 09:02:05 +0900

    add: リファクタリングとテストコードの作成

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		"localhost:8080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	)
-	if err != nil {
-		fmt.Printf("faild to terminate server: %v", err)
-		os.Exit(1)
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to teminate server: %v", err)
 	}
 }
 
-// branch-test
+func run(cxt context.Context) error {
+
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+
+	eg, ctx := errgroup.WithContext(cxt)
+	// 別ゴルーチンでHTTPサーバを起動
+	eg.Go(func() error {
+		// http.ErrServerClosed は http.Server.Shutdown() の正常終了なのでエラーではない
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルからの終了通知を受け取る
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	return eg.Wait()
+}
