bd7cefce5e444bb0836a607ce682f568fe12c2cb
Kohei Matsuo <koheiihphone@icloud.com>
2024-10-30 19:16:45 +0900

    Task型の定義とデータ永続化の仮実装

---
+++ store/store.go
@@ -0,0 +1,34 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/acmk189/todo_app_REST_API/entity"
+)
+
+var (
+	Tasks = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
+
+	ErrNotFound = errors.New("not found")
+)
+
+type TaskStore struct {
+	// 動作確認用
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (int, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return int(t.ID), nil
+}
+
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := *&entity.Tasks{}
+	for _, t := range ts.Tasks {
+		tasks = append(tasks, *t)
+	}
+	return tasks
+}
